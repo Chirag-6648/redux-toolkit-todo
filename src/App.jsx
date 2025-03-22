fbf5084903c41a6951ba49c11e4b0af0da756ba4
Chirag-6648 <rathodchirag1467@gmail.com>
2025-03-22 13:48:52 +0530

    Add Todo functionality with Redux and Tailwind CSS integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 
 function App() {
   return (
     <>
-      <h1>Hello this is Redux</h1>
+      <h1>Todo Using Redux</h1>
+      <AddTodo />
+      <Todos />
     </>
   );
 }
